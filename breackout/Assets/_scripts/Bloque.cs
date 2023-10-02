cde33ef18fc36aa823f5535afe0967c874488349
castaneda932 <castachatz90@outlook.com>
2023-10-02 16:50:35 -0600

    se añadieron los 2 GO con su material y sus scripts
    -añadimos 2 GO con su scripts, uno que haga que la bola se ralentice y otro que haga que el bloque explote y haga un spawn de una segunda bola

--- breackout/Assets/_scripts/Bloque.cs
+++ breackout/Assets/_scripts/Bloque.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cubo : MonoBehaviour
+public class Bloque : MonoBehaviour
 {
+    public int resistencia = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,17 @@ public class cubo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (resistencia <= 0)
+        {
+         Destroy(this.gameObject);
+        }
+ 
+
     }
+
+    public virtual void rebotarbola()
+    {
+
+    }
+
 }
---
+++ breackout/Assets/_scripts/Bloquemadera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloquemadera : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 3;
+    }
+
+    public override void rebotarbola()
+    {
+        base.rebotarbola();
+    }
+    // Update is called once per frame
+
+}
